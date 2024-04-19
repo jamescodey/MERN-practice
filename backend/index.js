0a38c3b948094f723874064e8b5d7acc7832ba56
jamescobybacasavila@gmail.com <james.avila@awsys-i.com>
2024-04-19 17:36:43 +0800

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express, { request } from "express";
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from "mongoose";
-import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js'
 
 
 const app = express();
@@ -13,43 +13,15 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to MERN Stack tutorial!');
 })
 
-// Routing for Save a new book
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: "Send all required fields: title, author, publishYear",
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook)
-        return response.status(201).send(book)
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({message: error.message});
-    }
-})
-
-// Route for requesting all books in database
-app.get();
-
-app.listen(PORT, () => {
-    console.log(`App is listening to port ${PORT}`)
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
         console.log("App is connected to database.");
+        app.listen(PORT, () => {
+            console.log(`App is listening to port ${PORT}`);
+        });
     })
     .catch((error) => {
         console.log(error.message);
