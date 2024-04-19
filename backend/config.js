ab573ab82845fb13d0bcab0df0d9c931db258e07
James Coby Avila <james.avila@awsys-i.com>
2024-04-19 16:16:46 +0800

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:MRf3EuWahgXNlr8B@mern-practice.n55rgnc.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Practice';
---
+++ backend/index.js
@@ -0,0 +1,56 @@
+import express, { request } from "express";
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack tutorial!');
+})
+
+// Routing for Save a new book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook)
+        return response.status(201).send(book)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({message: error.message});
+    }
+})
+
+// Route for requesting all books in database
+app.get();
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port ${PORT}`)
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App is connected to database.");
+    })
+    .catch((error) => {
+        console.log(error.message);
+    })
